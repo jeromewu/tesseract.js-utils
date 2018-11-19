b61c79cd91e496a84fb224623e19f48468fba6f2
Jerome Wu <jeromewus@gmail.com>
2018-11-19 22:30:17 +0800

    Add browser tests

---
+++ tests/browser/loadLang.test.js
@@ -0,0 +1,11 @@
+import { Selector } from 'testcafe';
+
+// eslint-disable-next-line
+fixture`loadLang`
+  .page`http://localhost:3000/tests/browser/loadLang.test.html`;
+
+test('Wait 30s for test to complete', async (t) => {
+  await t
+    .wait(30000)
+    .expect(Selector('#mocha-stats').find('li').nth(1).child('em').textContent).eql('3');
+});
